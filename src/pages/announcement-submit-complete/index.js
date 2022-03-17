95ebe2cd1a833d41a9b5be325f07a9e4f71d9457
pparkddo <ehdud3453@gmail.com>
2022-03-17 20:40:21 +0900

    Typo: 공고 입력 완료페이지 오타 수정

--- src/pages/announcement-submit-complete/index.js
+++ src/pages/announcement-submit-complete/index.js
@@ -25,7 +25,7 @@ function AnnouncementSubmitComplete(props) {
         <div className="row button-wrap">
           <div className="col-xs-12 col-sm-6">
             <button className="btn" onClick={moveToAnnouncementListPage}>
-              차수 목록 바로가기
+              공고 목록 바로가기
             </button>
           </div>
           <div className="col-xs-12 col-sm-6">
